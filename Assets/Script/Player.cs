efa1c32568de1d7cbd313af8b2af0634abc579b4
Elio Raphael <69924027+elio75006@users.noreply.github.com>
2022-06-09 16:17:49 +0200

    Player Animation when candy is detected
    +Player can eat the candy

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class Player : MonoBehaviour
+{
+    public int m_candyEaten = 0;
+    [SerializeField] UnityEvent _hasEatACandyEvent;
+    [SerializeField] float _candyDetectorRange = 3f;
+    [SerializeField] LayerMask _candyDetectorMask;
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+       Collider2D detectorCollider =  Physics2D.OverlapCircle(transform.position, _candyDetectorRange, _candyDetectorMask);
+        if (detectorCollider != null)
+        {
+            _animator.SetBool("CandyDetected", true);
+            Debug.Log("Blip bloup, Candy, in, range.");
+        }
+        else
+        {
+            _animator.SetBool("CandyDetected", false);
+
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+
+        if (collision.gameObject.CompareTag("Candy"))
+        {
+            collision.gameObject.SetActive(false);
+            m_candyEaten++;
+            _hasEatACandyEvent.Invoke();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, _candyDetectorRange);
+    }
+}
